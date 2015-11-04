9585a591fd05ea10fc6e58351d1156f3b7d62901
Andrew Reid East <andrew@andreweast.net>
2015-11-04 18:17:37 +0000

    add comments for % logic

--- BinaryTriangle/Program.cs
+++ BinaryTriangle/Program.cs
@@ -16,8 +16,8 @@ namespace BinaryTriangle
             for (int i = 0; i < size; i++)
             {
                 //Console.WriteLine("{0} {1} {2}", i, i % 2, ((i + 3) / 2) % 2);
-                if (i % 2 == 0)
-                    triangle = triangle + (((i + 3) / 2) % 2);
+                if (i % 2 == 0) //alternate adding a 0 or 1 to the front and then back of the string
+                    triangle = triangle + (((i + 3) / 2) % 2); //add two 0s in a row, and then two 1s (+3 starts the sequence at 0)
                 else
                     triangle = (((i + 3) / 2) % 2) + triangle;
                 Console.WriteLine(triangle);
